24becaa4585c3d26fac08e9dbfac6833e35bc24a
Your Name <you@example.com>
2025-05-28 17:48:01 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -13,36 +13,58 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const twilioClient = require('twilio')(accountSid, authToken);
 
-app.use(cors());
+// Allowed origins array
+const allowedOrigins = [
+  'https://apsit-chat-frontend.onrender.com',
+  'https://apsit-chat-backend.onrender.com',
+  'http://localhost:3000'
+];
+
+// CORS middleware with multiple origins support
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like Postman or curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Origin allowed
+    } else {
+      callback(new Error('CORS policy: This origin is not allowed'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+  res.send('Hello, World!');
 });
 
 app.post('/', (req, res) => {
-    const { message, user: sender, type, members } = req.body;
+  const { message, user: sender, type, members } = req.body;
 
-    if(type === 'message.new') {
-        members
-            .filter((member) => member.user_id !== sender.id)
-            .forEach(({ user }) => {
-                if(!user.online) {
-                    twilioClient.messages.create({
-                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
-                        messagingServiceSid: messagingServiceSid,
-                        to: user.phoneNumber
-                    })
-                        .then(() => console.log('Message sent!'))
-                        .catch((err) => console.log(err));
-                }
-            })
+  if(type === 'message.new') {
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if(!user.online) {
+          twilioClient.messages.create({
+            body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+            messagingServiceSid: messagingServiceSid,
+            to: user.phoneNumber
+          })
+          .then(() => console.log('Message sent!'))
+          .catch((err) => console.log(err));
+        }
+      });
 
-            return res.status(200).send('Message sent!');
-    }
+    return res.status(200).send('Message sent!');
+  }
 
-    return res.status(200).send('Not a new message request');
+  return res.status(200).send('Not a new message request');
 });
 
 app.use('/auth', authRoutes);
